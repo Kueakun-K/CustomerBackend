7e0d242abe72ef5db8a3f4fa8533b8b6ff298c8f
BLACK <blackkitten010@gmail.com>
2023-02-18 14:27:41 +0700

    read post data

--- src/main/java/com/example/customerbackend/controller/AuthController.java
+++ src/main/java/com/example/customerbackend/controller/AuthController.java
@@ -1,15 +1,27 @@
 package com.example.customerbackend.controller;
 
+import com.example.customerbackend.payload.LoginPayload;
 import com.example.customerbackend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+
+
 
     @Autowired
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginPayload loginPayload) {
+        return ResponseEntity.ok(loginPayload.getUsername() + loginPayload.getPassword());
+    }
+
 }
--- src/main/java/com/example/customerbackend/service/AuthService.java
+++ src/main/java/com/example/customerbackend/service/AuthService.java
@@ -4,4 +4,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+//    public String VerifyPassword( String password, String confirm_password){
+//
+//    }
+//    public String Login(String username, String password) {
+//
+//    }
 }
