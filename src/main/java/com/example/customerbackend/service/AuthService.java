620299f7ee7f5ed0e5ecfcf709f50fef9b9a668f
BLACK <blackkitten010@gmail.com>
2023-02-18 22:07:40 +0700

    change login controller, add AuthServiceTest

--- src/main/java/com/example/customerbackend/service/AuthService.java
+++ src/main/java/com/example/customerbackend/service/AuthService.java
@@ -1,20 +1,15 @@
 package com.example.customerbackend.service;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-
-//    public String VerifyPassword( String password, String confirm_password){
-//
-//    }
-    public String Login(String username, String password) {
+    public boolean Login(String username, String password) {
         if(username.equals("User")  && password.equals("1234")){
-            return "login successful";
+            return true;
         }
         else {
-            return "cannot login";
+            return false;
         }
     }
 }
